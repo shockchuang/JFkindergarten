453217b121bde8cf877e1b83675a5581201002ec
shockchuang <shockchuang@gmail.com>
2021-10-06 15:41:30 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+var cacheName = 'hello-JFkindergarten-pwa';
+var filesToCache = [
+  '/',
+    '/index.html',
+    '/assets/css/default.css',
+    '/assets/css/index.css',
+    '/assets/css/style.css',
+    '/assets/css/rwd.css',
+    '/assets/css/sweetalert.css',
+    '/assets/images/header-bg.jpg',
+    '/assets/js/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
